6c785e6bfbb98da9f03a221325bc7234ac521e12
Rupaya <aasim@rupx.io>
2020-02-23 00:02:29 +0000

    update networkid

--- apollo.sh
+++ apollo.sh
@@ -102,7 +102,7 @@ run() {
 
   rupaya \
     --bootnodes "enode://daea23c08fc591109d1478543ed06fc714bd2b380c1eb4bc81e0d0e2ce75d900cfa8010fe127028366c4d8ff5e132246a6268a884152785946c4d13dccf2980e@206.189.18.217:30301" --syncmode "full" --gcmode "archive" \
-    --datadir ${DATADIR}/${NAME} --networkid 308 --port $PORT \
+    --datadir ${DATADIR}/${NAME} --networkid 77 --port $PORT \
     --announce-txs \
     --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 7050 --rpcvhosts "*" \
     --ws --wsaddr 0.0.0.0 --wsport 8050 --wsorigins "*" \
